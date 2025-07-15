8f1263a885238d2175c1b55cf4eefd93c3a39654
Vinayak <2021wc86299@wilp.bits-pilani.ac.in>
2025-07-15 20:02:12 +0530

    Fixed App.java syntax for Jenkins CI test

--- lab4project/src/main/java/com/devops/lab/App.java
+++ lab4project/src/main/java/com/devops/lab/App.java
@@ -2,12 +2,10 @@ package com.devops.lab;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello from Jenkins CI Trigger!");
     }
 }
+
